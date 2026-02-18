7a3fc0b4fd3c4822318b5d557e56171a48ac44e4
Hrishikesh Giri <goswamirishi83@gmail.com>
2026-02-19 02:13:38 +0530

    fix: update emotion service url handling + requirements.txt

--- backend/src/utils/emotionService.js
+++ backend/src/utils/emotionService.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export const detectEmotion = async (text) => {
   try {
+    const ML_API_URL = process.env.ML_API_URL || "http://127.0.0.1:8001";
     console.log("Sending text to ML service:", text);
-    const response = await axios.post("http://127.0.0.1:8001/predict_emotion", { text });
+    const response = await axios.post(`${ML_API_URL}/predict_emotion`, { text });
     console.log("ML Service Response:", response.data);
     return response.data.emotion;
   } catch (error) {
